e21bb021c13f4949c871f68016400ca238208050
rodcill <rodcill@proton.me>
2025-01-10 15:36:10 +0700

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-// script.js
-document.querySelectorAll('.copy-icon').forEach(icon => {
-  icon.addEventListener('click', () => {
-    const text = icon.previousSibling.textContent.trim();
-    navigator.clipboard.writeText(text);
-    alert(`Copied: ${text}`);
-  });
-});
